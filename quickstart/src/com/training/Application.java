54830f89ea13cfff5dfdb87a794d568e27e6effc
Sriharish.S <Sriharish.S@EXIND3010-87.allfon.in>
2022-04-07 11:14:49 +0530

    First Commit

---
+++ quickstart/src/com/training/Application.java
@@ -0,0 +1,17 @@
+package com.training;
+
+public class Application extends Harish {
+
+	public static void main(String[] args) {
+		Harish h=new Harish();
+		
+		
+		System.out.println(h.run());
+	//	System.out.println(h.run());
+		System.out.println(h.num);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ quickstart/src/com/training/Harish.java
@@ -0,0 +1,16 @@
+package com.training;
+public class Harish
+{
+	private static String run() {
+		return "Sriharish";
+
+	}
+	protected String getInfo() {
+		return "Welcome to Java Programming";
+	}
+		
+
+}
+
+
+
