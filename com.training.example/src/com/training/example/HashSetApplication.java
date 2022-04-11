8d4471c90a15d8e84a53c5ea27330a7c9b6ddc22
Sriharish.S <Sriharish.S@EXIND3010-87.allfon.in>
2022-04-11 17:33:37 +0530

    11/04/2022

---
+++ com.training.example/src/com/training/example/HashSetApplication.java
@@ -0,0 +1,30 @@
+package com.training.example;
+
+import java.util.HashSet;
+import java.util.*;
+
+public class HashSetApplication {
+
+	public static void main(String[] args) { 
+		
+		Student  ram=new Student(101,"ram",78);
+		Student shyam = new Student(102, "shyam", 68);
+		Student magesh=new Student(103,"Magesh",88);
+		Student chinnaMagesh = new Student(103,"Magesh",88);
+		
+		
+		Set<Student> set= new HashSet<>();
+		
+		set.add(magesh);
+		set.add(chinnaMagesh);
+		set.add(shyam);
+		set.add(ram);
+		
+		System.out.println(set);
+		
+		System.out.println(set.size());
+		
+	
+	}
+
+}
