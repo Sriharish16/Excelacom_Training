823f09c11619b5fbf9880ab60330d71f9e6a3144
Sriharish.S <Sriharish.S@EXIND3010-87.allfon.in>
2022-05-13 10:04:27 +0530

    13/05/2022

---
+++ Spring-data-jpa-bidirectional/src/main/java/com/example/demo/ifaces/bidirectional/DoctorRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.ifaces.bidirectional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DoctorRepository extends JpaRepository<T, ID> {
+
+}
