99677d35e30ac9a3ef6ed7f92ca83f23b11923a4
Sriharish.S <Sriharish.S@EXIND3010-87.allfon.in>
2022-05-24 16:53:49 +0530

    24/05/2022

--- Config-client/src/main/java/com/example/demo/ConfigClientApplication.java
+++ Config-client/src/main/java/com/example/demo/ConfigClientApplication.java
@@ -1,26 +1,31 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.ConfigurableApplicationContext;
+import com.example.demo.repository.DoctorRepository;
 
 @SpringBootApplication
-@RestController
+//@RestController
 
 public class ConfigClientApplication {
 
-	
-	@Value("${customer.firstName}")
-	private String name;
-	
-	@GetMapping(path ="/names")
-	public String getName() {
-		return this.name;
-	}
+//	
+//	@Value("${customer.firstName}")
+//	private String name;
+
+//	@GetMapping(path ="/names")
+//	public String getName() {
+//		return this.name;
+//	}
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigClientApplication.class, args);
+		ConfigurableApplicationContext ctx=SpringApplication.run(ConfigClientApplication.class, args);
+		
+		DoctorRepository repo =ctx.getBean(DoctorRepository.class);
+		repo.findAll().forEach(System.out::println);
+		
+	
+		
 	}
 
 }
